e25ae8f03c0d57ffe37b7d40c9468918eeb1a6de
joswin18 <gamrgen18@gmail.com>
2025-01-05 20:14:46 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -21,9 +21,11 @@ function App() {
           <div className='Transaction'>
             <div className="left">
               <div className="name">new Samsung tv</div>
+              <div className="description">time to buy new tv</div>
             </div>
             <div className="right">
-
+              <div className="price">$500</div>
+              <div className="datetime">2025-1-5</div>
             </div>
           </div>
       </div>
