1b9b3e661a9aa340aa390ebc29d80c45abef2bde
joswin18 <gamrgen18@gmail.com>
2025-01-24 22:04:49 +0530

    backend/api

---
+++ api/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+app.get('/api/test',(req,res)=>{
+    res.json('test ok')
+})
+
+app.post('/api/transaction',(req,res)=>{
+    res.json(req.body)
+})
+
+
+app.listen(4000)
